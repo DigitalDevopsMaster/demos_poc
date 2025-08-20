6b12be450c9ae7e639b3b79945663b2779b55e89
Isaac <isaacsauri@digitaldevops.com.mx>
2025-08-20 12:27:54 -0600

    fix: adjust layout height calculations to account for header spacing

--- hmoissan/v2/components/hmoissan-layout.js
+++ hmoissan/v2/components/hmoissan-layout.js
@@ -105,13 +105,14 @@ class HmoissanLayout extends HTMLElement {
     style.textContent = `
       .hmoissan-layout {
         width: 100%;
-        height: 100%;
+        height: calc(100% - 60px);
+
       }
       
       .layout-content {
         display: flex;
         flex-direction: column;
-        height: calc(100% - 120px);
+        height: 100%;
         position: relative;
       }
       
